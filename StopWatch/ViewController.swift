d62f905ba2d924917f78383cc1424be46aa163cc
yinweisu <wyin@ucdavis.edu>
2018-10-05 09:58:56 -0700

    Swift Version Converted

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -9,17 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    @IBAction func startClick() {
+        self.toggleButton(withTitles: ("Start", "Stop"), on: startButton)
+    }
+    
+    func toggleButton(withTitles titles: (String, String), on button: UIButton) {
+        let title = button.currentTitle == titles.0 ? titles.1 : titles.0
+        button.setTitle(title, for: .normal)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
